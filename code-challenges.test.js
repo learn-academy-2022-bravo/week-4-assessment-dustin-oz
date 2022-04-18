3b898f37fbd39a33c22f57f756bbe84ccbeb97ad
dustin-oz <schelldd@gmail.com>
2022-04-18 08:48:12 -0500

    Finished 2nd code challenge

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,14 +15,14 @@
 
 // a) Create a test with an expect statement using the variable provided.HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-describe('arrayShuffler', () => {
-    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-    it('takes in an array, removes the first item from the array and shuffles the remaining content.', () => {
-        expect(arrayShuffler(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]));
-        expect(arrayShuffler(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]));
-    })
-})
+// describe('arrayShuffler', () => {
+//     const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+//     const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+//     it('takes in an array, removes the first item from the array and shuffles the remaining content.', () => {
+//         expect(arrayShuffler(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]));
+//         expect(arrayShuffler(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]));
+//     })
+// })
 
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
@@ -65,13 +65,44 @@ const arrayShuffler = (array) => {
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
+describe('minMax', () => {
+    const nums1 = [3, 56, 90, -8, 0, 23, 6]
+    const nums2 = [109, 5, 9, 67, 8, 24]
+    it('takes an array of numbers and returns an array of the minimum and maximum numbers in that order.', () => {
+        expect(minMax(nums1)).toEqual(expect.arrayContaining([-8, 90]));
+        expect(minMax(nums2)).toEqual(expect.arrayContaining([5, 109]));
+    })
+})
+
 // Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
 // Expected output: [5, 109]
 
+        // FAIL./ code - challenges.test.js
+        // minMax
+        //     ✕ takes an array of numbers and returns an array of the minimum and maximum numbers in that order. (1 ms)
+
+        //   ● minMax › takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+
+        //     ReferenceError: minMax is not defined
 
 // b) Create the function that makes the test pass.
+
+// --> Pseudo Code <--
+// Create function called minMax
+// Set variable with the min value in array with math.min
+// Set variable with max value with math.max
+// return a result that is a combo of values
+
+const minMax = (array) => {
+    let minimum = Math.min(...array)
+    let maximum = Math.max(...array)
+    let result = ([minimum, maximum])
+    return result
+}
+
+        // PASS./ code - challenges.test.js
+        // minMax
+        //     ✓ takes an array of numbers and returns an array of the minimum and maximum numbers in that order. (3 ms)
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
